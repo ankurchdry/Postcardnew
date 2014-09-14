1df5b44e82514dc4fb6cf0f54406ecac50828d58
ankurchdry <ankurchdry@ANKUR-CHOUDHARYs-MacBook-Pro.local>
2014-09-14 16:05:11 -0400

    Added some more functionality

--- Postcard - New/ViewController.swift
+++ Postcard - New/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
    
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var enterNameTextField: UITextField!
     
     @IBOutlet weak var enterMessageField: UITextField!
@@ -29,12 +31,24 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonSend(sender: UIButton) {
-        //Code will Evaluate when button is pressed.
-        //Adding a commit
+        //Code will Evaluate when button is pressed, the action for message label
         messageLabel.hidden = false
         messageLabel.text = enterMessageField.text
         enterMessageField.resignFirstResponder()
         messageLabel.textColor = UIColor.redColor()
+        enterMessageField.text = ""
+
+        
+        //Now we need to add Name related lable code
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        enterNameTextField.resignFirstResponder()
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+
+        //This is the button message change
+        
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
 
